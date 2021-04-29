7e0615270a7350fb9d0e88031479ebcd840cd104
Doğukan Atakul <datakul@yandex.com>
2021-04-29 09:52:31 +0300

    Update decode.php

--- decode.php
+++ decode.php
@@ -16,6 +16,22 @@ function cryptoJsAesDecrypt($passphrase, $jsonString)
     $data = openssl_decrypt($ct, 'aes-256-cbc', $key, true, $iv);
     return json_decode($data, true);
 }
+                        
+function cryptoJsAesEncrypt($passphrase, $value)
+{
+    $salt = openssl_random_pseudo_bytes(8);
+    $salted = '';
+    $dx = '';
+    while (strlen($salted) < 48) {
+        $dx = md5($dx . $passphrase . $salt, true);
+        $salted .= $dx;
+    }
+    $key = substr($salted, 0, 32);
+    $iv = substr($salted, 32, 16);
+    $encrypted_data = openssl_encrypt(json_encode($value), 'aes-256-cbc', $key, true, $iv);
+    $data = array("ct" => base64_encode($encrypted_data), "iv" => bin2hex($iv), "s" => bin2hex($salt));
+    return json_encode($data);
+}
         
 
 $crypto = '{"ct":"Sx/Ltl0lv0j6Rqs8eoiVPw==","iv":"88c39199b21de4914c01f414b6f79b28","s":"4b9b8f840cbba0bb"}';
